9de9351d074d08c51f03628ab98f7f39ae7aabb8
NARJAM <n4rj4m@gmail.com>
2020-05-04 22:32:41 +0530

    Update friendly model changes

---
+++ JAMLib/GameModels.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using JINADA;
+
+    [Serializable]
+    public struct WorldStateModel
+    {
+    }
+
+    [System.Serializable]
+    public struct PlayerStateModel
+    {
+    }
+
+    [System.Serializable]
+    public struct PlayerInputModel
+    {
+    }
+
+    [System.Serializable]
+    public struct PlayerInitModel
+    {
+
+    }
